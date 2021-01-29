1927155b3968ece1800fb46863e10fa1f5ed9cfb
unknown <you@example.com>
2021-01-29 16:44:12 -0300

    29/01/21

--- JS/aula2/resources/js/exercicios/exercicio35.js
+++ JS/aula2/resources/js/exercicios/exercicio35.js
@@ -1,3 +1,9 @@
 function exercicio35() {
-    
+    for(let i = 0;i <=100;i++) {
+        if(i % 2 != 0) {
+            document.write(`${i}, `)
+        }
+    }
 }
+
+exercicio35()
---
+++ JS/aula2/resources/js/exercicios/exercicio37.js
@@ -0,0 +1,12 @@
+function exercicio37() {
+    for(let num = 1; num <= 15; num++) {
+        parseInt(window.prompt('Digite 15 números'))
+        if(num < num) {
+            document.write(`<h1>O menor número é: ${num}<h1>`)
+        }
+    }
+}
+
+exercicio37()
+
+
